5b5f3c33c13885b517d251cec921bd67a524b523
Macailh <salvadorenriquegb@gmail.com>
2023-07-01 20:49:28 -0600

    feat: add upload file

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ import gofile
 def upload_file(file):
     server = gofile.getServer()
     print(f"The server is {server}")
+
+    url = gofile.uploadFile(file)
+    print(f"The link of the file is {url}")
+
+
+if __name__ == "__main__":
+    upload_file("exampleFile.txt")
