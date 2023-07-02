c400706cd8a8b42991aa2e607888aad20d6ce4d9
Macailh <salvadorenriquegb@gmail.com>
2023-07-02 15:45:30 -0600

    refactor: clean print output

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ app = typer.Typer()
 @app.command()
 def upload(file):
     server = gofile.getServer()
-    print(f"The server is {server}")
+    print(f"The server is {server['server']}")
 
     url = gofile.uploadFile(file)
-    print(f"The link of the file is {url}")
+    print(f"The link of the file is {url['downloadPage']}")
 
 
 if __name__ == "__main__":
